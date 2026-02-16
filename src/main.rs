b55324b6e31f0d495c52bd5c545be7c8f4a5cff4
Liam Strand <ltrs@u.northwestern.edu>
2026-02-15 23:01:35 -0600

    initialize and use scheduler

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use saccade::event_library::EventLibrary;
 use saccade::event_registry::EventRegistry;
 use saccade::oculomotor::Oculomotor;
 use saccade::perf::Perf;
+use saccade::scheduler::random::RandomScheduler;
 use saccade::scheduler::Scheduler;
-use saccade::scheduler::test::TestScheduler;
 use saccade::syscalls;
 use std::fs::File;
 use std::io::BufReader;
@@ -68,7 +68,7 @@ fn main() -> std::io::Result<()> {
             let pid = child.id();
             let thread = thread::spawn(move || {
                 let registry = EventRegistry::new(lib);
-                let mut scheduler = TestScheduler::new(&registry);
+                let mut scheduler = RandomScheduler::default();
                 scheduler.init(registry.get_event_ids());
                 let mut oculomotor = Oculomotor::new(
                     pid,
